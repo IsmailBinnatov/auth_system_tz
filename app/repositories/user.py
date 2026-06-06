58a3934b33d759ce3ac4a0135f68547fa60fbe3e
Ismail <ismll.s@mail.ru>
2026-06-06 21:57:26 +0400

    feat(repo): add get_user_by_email, create_user, assign_role_to_user methods to user repository

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models.models import User
+from app.models.models import User, Role, UserRole
 
 
 class UserRepository:
@@ -13,7 +13,6 @@ class UserRepository:
             select(User)
             .where(User.id == user_id)
         )
-
         user = (await self.db.execute(query)).scalar_one_or_none()
         return user
 
@@ -23,6 +22,36 @@ class UserRepository:
             .where(User.id == user_id)
             .where(User.is_active == True)
         )
-
         user = (await self.db.execute(query)).scalar_one_or_none()
         return user
+
+    async def get_user_by_email(self, email: str) -> User | None:
+        query = (
+            select(User)
+            .where(User.email == email)
+        )
+        user = (await self.db.execute(query)).scalar_one_or_none()
+        return user
+
+    async def create_user(self, user: User) -> User:
+        self.db.add(user)
+        return user
+
+    async def assign_role_to_user(self, user_id: int, role_name: str) -> None:
+        query = (
+            select(Role)
+            .where(Role.name == role_name)
+        )
+        role = (await self.db.execute(query)).scalar_one_or_none()
+        if not role:
+            raise ValueError(f'Role \'{role_name}\' not found')
+
+        user_role = UserRole(
+            user_id=user_id,
+            role_id=role.id,
+        )
+
+        self.db.add(user_role)
+
+    async def flush(self):
+        await self.db.flush()
